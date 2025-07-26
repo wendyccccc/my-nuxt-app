16d2be8dada21b0f1458ded7cdd8a359c482cded
wendy <lienyun.chang@gmail.com>
2025-07-26 23:00:46 +0800

    auto import vue api

--- pages/[id].vue
+++ pages/[id].vue
@@ -3,8 +3,11 @@
 </template>
 
 <script setup>
+
 console.log('Dynamic route page loaded');
 // 動態網址
 // nuxt2: /pages/_id.vue
 // nuxt3: /pages/[id].vue
+
+const route = useRoute(); // auto imported 跟vue有關的api都可以直接使用
 </script>
