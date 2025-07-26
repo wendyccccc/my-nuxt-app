559161a809de2a6b5141faaa64ad42275cacfd3a
wendy <lienyun.chang@gmail.com>
2025-07-26 22:59:29 +0800

    動態網址

---
+++ pages/[id].vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>動態路由頁面: {{ $route.params.id }}</h1>
+</template>
+
+<script setup>
+console.log('Dynamic route page loaded');
+// 動態網址
+// nuxt2: /pages/_id.vue
+// nuxt3: /pages/[id].vue
+</script>
